ded7065e7986c58ea9a5293910d6335cae47945e
jimmy <kji0205@gmail.com>
2017-04-17 00:04:03 +0900

    .

---
+++ a.c
@@ -0,0 +1,11 @@
+
+int main(void)
+{
+  int i=0;
+  int Count=0;
+  // Node* List=NULL;
+  for (i=0;i<5;i++){
+    printf("\nTest Loop");
+  }
+  return 0;
+}
---
+++ array.js
@@ -0,0 +1,19 @@
+var sing = ['박정현1', '김범수2', '빅뱅3', '콜드플레이4'];
+
+console.log('---ori---');
+console.log(sing);
+console.log('---sort---');
+console.log(sing.sort());
+console.log('---reverse---');
+console.log(sing.reverse());
+// console.log(sing.slice(0,1));
+// console.log(sing.slice(0,2));
+// console.log(sing.slice(0,3));
+// console.log(sing);
+console.log('---slice---');
+console.log(sing.slice(-1, -1));
+// console.log(sing);
+console.log('---splice---');
+console.log(sing.splice(1));
+console.log(sing);
+
