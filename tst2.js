64b11bc4493a8f0e881aa193fa243df75a19aa3e
jimmy <kji0205@gmail.com>
2017-04-23 21:06:38 +0900

    detail

---
+++ tst2.js
@@ -0,0 +1,19 @@
+var EventHandler = function(){
+    var body = document.body;
+    var prdOptionClick = function(){}
+}
+
+var data = {
+    name: 'test product1',
+    brandName: 'brd',
+    price: 30000,
+    optionName: 'black'
+}
+var PrdInfoController = function() {
+    getPrdList = function(){},
+    getPrdOptionList = function(){},
+};
+
+var PrdDetailController = function() {
+    var getPrdList = function(){}
+}
